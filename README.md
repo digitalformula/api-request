d4ecc3ffc371f2b4ef231f5bb3f86d226ce67ca7
Chris Rasmussen <chris@digitalformula.net>
2016-11-08 15:35:06 +1100

    Added API version control, added body encoding option

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # digitalformula/api-request
 
 Packagist package for creating a basic API request via GuzzleHttp
+
+## ChangeLog
+
+2016-11-08
+
+- Added option to control whether or not POST request bodies are encoded before being sent
+- Added $version
+- Changed all constructor parameters to required (some were optional), except $version
--- src/DigitalFormula/ApiRequest/ApiRequest.php
+++ src/DigitalFormula/ApiRequest/ApiRequest.php
@@ -22,120 +22,135 @@ namespace DigitalFormula\ApiRequest;
 class ApiRequest
 {
 
-	/**
-	 * The username to use during the connection
-	 */
-	var $username;
+    /**
+     * The username to use during the connection
+     */
+    var $username;
 
-	/**
-	 * The password to use during the connection
-	 */
-	var $password;
+    /**
+     * The password to use during the connection
+     */
+    var $password;
 
-	/**
-	 * The path for the actual API request
-	 */
-	var $requestPath;
+    /**
+     * The path for the actual API request
+     */
+    var $requestPath;
 
-	/**
-	 * The IP address of the CVM
-	 */
-	var $cvmAddress;
+    /**
+     * The IP address of the CVM
+     */
+    var $cvmAddress;
 
-	/**
-	 * The port to connect on
-	 */
-	var $cvmPort;
+    /**
+     * The port to connect on
+     */
+    var $cvmPort;
 
-	/**
-	 * The timeout period i.e. how long to wait before the request is considered failed
-	 */
-	var $connectionTimeout;
+    /**
+     * The timeout period i.e. how long to wait before the request is considered failed
+     */
+    var $connectionTimeout;
 
-	/**
-	 * Is this a GET or POST request?
-	 */
-	var $method;
+    /**
+     * Is this a GET or POST request?
+     */
+    var $method;
 
-	/**
-	 * @var \GuzzleHttp\Client
-	 */
-	var $client;
+    /**
+     * @var \GuzzleHttp\Client
+     */
+    var $client;
 
-	/**
-	 * apiRequest constructor.
-	 * @param $username
-	 * @param $password
-	 * @param $cvmAddress
-	 * @param string $cvmPort
-	 * @param int $connectionTimeout
-	 * @param string $requestPath
-	 * @param string $method
-	 */
-	public function __construct( $username, $password, $cvmAddress, $cvmPort = '9440', $connectionTimeout = 3, $requestPath = '/PrismGateway/services/rest/v1', $method = 'GET' )
-	{
-		$this->username          = $username;
-		$this->password          = $password;
-		$this->requestPath       = $requestPath;
-		$this->cvmAddress        = $cvmAddress;
-		$this->cvmPort           = $cvmPort;
-		$this->connectionTimeout = $connectionTimeout;
-		$this->method            = $method;
+    /**
+     * @var version
+     *
+     * The version that will be used in the API request (for future use)
+     *
+     */
+    var $version;
 
-		$credentials = base64_encode( $this->username . ':' . $this->password );
+    /**
+     * apiRequest constructor.
+     * @param $username
+     * @param $password
+     * @param $cvmAddress
+     * @param string $cvmPort
+     * @param int $connectionTimeout
+     * @param string $requestPath
+     * @param string $method
+     * @param string $version
+     */
+    public function __construct( $username, $password, $cvmAddress, $cvmPort, $connectionTimeout, $requestPath, $method, $version = 'v3' )
+    {
+        $this->username          = $username;
+        $this->password          = $password;
+        $this->requestPath       = $requestPath;
+        $this->cvmAddress        = $cvmAddress;
+        $this->cvmPort           = $cvmPort;
+        $this->connectionTimeout = $connectionTimeout;
+        $this->method            = $method;
 
-		$this->client = new \GuzzleHttp\Client( [
-			'base_url' => sprintf( "https://%s:%s%s",
-				$this->cvmAddress,
-				$this->cvmPort,
-				$this->requestPath
-			),
-			'defaults' => [
-				'auth' => [
-					$this->username,
-					$this->password,
-				],
-				'cookies' => true,
-				'config' => [
-					'timeout' => $this->connectionTimeout,
-					'connect_timeout' => $this->connectionTimeout,
-					'curl' => [
-						CURLOPT_SSL_VERIFYHOST => false,
-						CURLOPT_SSL_VERIFYPEER => false
-					]
-				],
-				'headers' => [
-					"Accept" => "application/json",
-					"Content-Type" => "application/json",
-					"Authorization" => "Basic $credentials"
-				],
-			]
-		] );
+        $credentials = base64_encode( $this->username . ':' . $this->password );
 
-	}
+        $this->client = new \GuzzleHttp\Client( [
+            'base_url' => sprintf( "https://%s:%s%s",
+                $this->cvmAddress,
+                $this->cvmPort,
+                $this->requestPath
+            ),
+            'defaults' => [
+                'auth' => [
+                    $this->username,
+                    $this->password,
+                ],
+                'cookies' => true,
+                'config' => [
+                    'timeout' => $this->connectionTimeout,
+                    'connect_timeout' => $this->connectionTimeout,
+                    'curl' => [
+                        CURLOPT_SSL_VERIFYHOST => false,
+                        CURLOPT_SSL_VERIFYPEER => false
+                    ]
+                ],
+                'headers' => [
+                    "Accept" => "application/json",
+                    "Content-Type" => "application/json",
+                    "Authorization" => "Basic $credentials"
+                ],
+            ]
+        ] );
 
-	/**
-	 * Perform an API GET request
-	 *
-	 * @param $requestPath
-	 * @return \GuzzleHttp\Message\FutureResponse|\GuzzleHttp\Message\ResponseInterface|\GuzzleHttp\Ring\Future\FutureInterface|null
-	 */
-	public function get( $requestPath )
-	{
-		return $this->client->get( $requestPath );
-	}
+    }
 
-	/**
-	 * Perform an API POST request i.e. make changes
-	 *
-	 * @param $requestPath
-	 * @param $body
-	 * @return \GuzzleHttp\Message\FutureResponse|\GuzzleHttp\Message\ResponseInterface|\GuzzleHttp\Ring\Future\FutureInterface|null
-	 */
-	public function post( $requestPath, $body )
-	{
-		return $this->client->post( $requestPath, [ 'body' => json_encode( $body ) ] );
-	}
+    /**
+     * Perform an API GET request
+     *
+     * @param $requestPath
+     * @return \GuzzleHttp\Message\FutureResponse|\GuzzleHttp\Message\ResponseInterface|\GuzzleHttp\Ring\Future\FutureInterface|null
+     */
+    public function get( $requestPath )
+    {
+        return $this->client->get( $requestPath );
+    }
+
+    /**
+     * Perform an API POST request i.e. make changes
+     *
+     * @param $requestPath
+     * @param $body
+     * @return \GuzzleHttp\Message\FutureResponse|\GuzzleHttp\Message\ResponseInterface|\GuzzleHttp\Ring\Future\FutureInterface|null
+     */
+    public function post( $requestPath, $body, $encode = true )
+    {
+        if( $encode ) {
+            return $this->client->post( $requestPath, [ 'body' => json_encode( $body ) ] );
+        }
+        else {
+            return $this->client->post( $requestPath, [ 'body' => $body ] );
+        }
+
+    }
 
 }
 /* ApiRequest */
